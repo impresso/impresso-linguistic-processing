120bd8af757093f89b8bf1738ed01c71531f4f25
Simon Clematide <simon.clematide@uzh.ch>
2024-11-23 16:41:38 +0100

    feat: add git_version for lingproc

--- lib/spacy_linguistic_processing.py
+++ lib/spacy_linguistic_processing.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-Script to preprocess text for topic modeling, utilizing precomputed language identification results.
+Script to_ preprocess text for topic modeling, utilizing precomputed language
+identification results.
 """
 
 import argparse
@@ -10,21 +11,28 @@ import json
 import logging
 import os
 from datetime import datetime
-from typing import Generator, Dict, Optional, Any, IO
+from typing import Any, Dict, Generator, IO, Optional
 
+import boto3
 import dotenv
 import jsonschema
+import smart_open
+import spacy
 
 dotenv.load_dotenv()
 
 
-import spacy
-import smart_open
-
 log = logging.getLogger(__name__)
 
+SCHEMA_BASE_URI = (
+    "https://impresso.github.io/impresso-schemas/json/linguistic_annotation/"
+)
+
+IMPRESSO_SCHEMA = "ling_spacy.schema.json"
+
+
 # TAG map for lb language processing
-TAG_MAP = {
+LB_TAG_MAP = {
     "$": "PUNCT",
     "ADJ": "ADJ",
     "AV": "ADV",
@@ -45,7 +53,7 @@ TAG_MAP = {
 }
 
 
-def get_s3_client():  # -> boto3.client
+def get_s3_client() -> boto3.client:
     """Returns a boto3.client object for interacting with S3.
 
     Returns:
@@ -65,16 +73,17 @@ def get_s3_client():  # -> boto3.client
 
 def get_next_doc(
     infile: str, client: Optional[Any] = None
-) -> Generator[Dict[str, any], None, None]:
+) -> Generator[Dict[str, Any], None, None]:
     """
     Generates documents from a file line by line.
 
     Args:
-        infile (str): The path to the input file.
-        client (Optional[Any]): The S3 client to use for reading the file. Defaults to None.
+        infile (str):a The path to the input file.
+        client (Optional[Any]): The S3 client to use for reading the file.
+            Defaults to None.
 
     Yields:
-        Generator[Dict[str, any], None, None]: A generator yielding documents as
+        Generator[Dict[str, Any], None,t None]: A generator yielding documents as
             dictionaries.
     """
     transport_params = {}
@@ -85,24 +94,24 @@ def get_next_doc(
             yield json.loads(line)
 
 
-def output_doc(doc: Dict[str, any], out_file: "IO[str]") -> None:
+def output_doc(doc: Dict[str, Any], out_file: "IO[str]") -> None:
     """
     Outputs a document to the specified file.
 
     Args:
-        doc (Dict[str, any]): The document to output.
+        doc (Dict[str, Any]): The document to output.
         out_file (IO[str]): The file object to write the document to.
     """
     print(json.dumps(doc, ensure_ascii=False, separators=(",", ":")), file=out_file)
 
 
-def read_langident(path: str, client: Optional[Any]) -> Dict[str, str]:
+def read_langident(path: str, client: Optional[Any] = None) -> Dict[str, str]:
     """
     Reads language identification results from a file.
 
     Args:
-        path (str): The (s3) path to the file containing language identification results.
-        client (Optional[Any]): The S3 client to use for reading the file. Defaults to None.
+        path (str): The (s3) path to the language identification file.
+        client (Optional[Any]): The S3 client to use for reading the file.
     Returns:
         Dict[str, str]: A dictionary mapping document IDs to their identified languages.
     """
@@ -168,9 +177,14 @@ class LinguisticProcessing:
             else None
         )
         self.model_versions: Dict[str, str] = {}  # Store model versions
+        self.git_version = (
+            self.args.git_version
+            if self.args.git_version
+            else os.environ.get("GIT_VERSION", "unknown")
+        )
         if self.args.validate:
             with smart_open.open(
-                "https://impresso.github.io/impresso-schemas/json/linguistic_annotation/ling_spacy.schema.json",
+                SCHEMA_BASE_URI + IMPRESSO_SCHEMA,
                 "r",
             ) as f:
                 self.schema = json.load(f)
@@ -178,7 +192,7 @@ class LinguisticProcessing:
                 schema=self.schema,
                 resolver=jsonschema.RefResolver(
                     referrer=self.schema,
-                    base_uri="https://impresso.github.io/impresso-schemas/json/linguistic_annotation/",
+                    base_uri=SCHEMA_BASE_URI,
                 ),
             )
 
@@ -197,40 +211,70 @@ class LinguisticProcessing:
             nlp.add_pipe("sentencizer", first=True)
             nlp.max_length = 100000
             self.language_proc_units[lang] = nlp
-            self.model_versions[lang] = nlp.meta["version"]  # Store model version
+            self.model_versions[lang] = (
+                "spacy@"
+                + spacy.__version__
+                + ":"
+                + nlp.meta["lang"]
+                + "_"
+                + nlp.meta["name"]
+                + "@"
+                + nlp.meta["version"]
+                + ":"
+                + "|".join(nlp.pipe_names)
+            )
             log.info("LOADED PIPELINE %s %s", nlp, nlp.pipeline)
+            log.info("model_id: %s", self.model_versions[lang])
         else:
             log.error("No model found for %s", lang)
 
     def process_doc(
         self,
-        json_obj: Dict[str, any],
+        json_obj: Dict[str, Any],
         timestamp: str,
-    ) -> Optional[Dict[str, any]]:
+    ) -> Optional[Dict[str, Any]]:
         """
         Processes a single document, adding linguistic annotations.
 
         Args:
-            json_obj (Dict[str, any]): The document to process.
-            language_proc_units (Dict[str, spacy.language.Language]): The language processing units.
+            json_obj (Dict[str, Any]): The document to process.
             timestamp (str): The current timestamp.
 
         Returns:
-            Optional[Dict[str, any]]: The processed document or None if processing fails.
+            Optional[Dict[str, Any]]: The processed document or None if processing
+                fails.
         """
         docid = json_obj["id"]
+
+        full_text = json_obj.get(self.args.text_property)
+        if full_text is None:
+            log.warning(
+                "Full text property `%s` unavailable in `%s`",
+                self.args.text_property,
+                docid,
+            )
+            return None
+        full_text_len = len(full_text)
+        if full_text_len == 0:
+            log.info("Empty text: %s", docid)
+            self.stats["CONTENT-ITEMS-EMPTY"] += 1
+            return None
+        elif full_text_len < self.args.min_doc_length:
+            log.info("Short text (%s chars): %s ", full_text_len, docid)
+            self.stats["CONTENT-ITEMS-SHORT"] += 1
+            return None
+
         lang = None
+        lid_path = "default"
 
         if self.args.language:
             lang = self.args.language
             self.stats["LANG-FROM-ARG"] += 1
         elif self.lang_ident_data:
-
-            lang = self.lang_ident_data.get(
-                docid
-            )  # Use .get() to handle missing IDs gracefully
+            lang = self.lang_ident_data.get(docid)
             if lang:
                 self.stats["LANG-FROM-LID"] += 1
+                lid_path = self.args.lid
         if lang is None:
             lang = json_obj.get("lg")
             if lang:
@@ -240,7 +284,7 @@ class LinguisticProcessing:
                 log.warning(
                     "Skipping %s. Language is None. Text: `%s`",
                     docid,
-                    json_obj.get("ft", "")[:100],
+                    json_obj.get("ft", "")[:50],
                 )
                 return None
 
@@ -252,19 +296,7 @@ class LinguisticProcessing:
         if lang not in self.language_proc_units:
             self.create_lpu(lang)
 
-        try:
-            full_text = json_obj[self.args.text_property]
-        except KeyError:
-            log.error("No full text found for %s", docid)
-            return None
-        if len(full_text) < self.args.min_doc_length:
-            log.warning(
-                "Document %s too short (%d): %s", docid, len(full_text), full_text
-            )
-            self.stats["CONTENT-ITEMS-SHORT"] += 1
-            return None
-        else:
-            self.stats["CONTENT-ITEMS-OK"] += 1
+        self.stats["CONTENT-ITEMS-OK"] += 1
         preprocessed_text = []
         doc = self.language_proc_units[lang](full_text)
 
@@ -275,7 +307,7 @@ class LinguisticProcessing:
                 tok_dict = {
                     "t": tok.text,
                     "p": (
-                        TAG_MAP.get(tok.tag_, "X") if lang == "lb" else tok.pos_
+                        LB_TAG_MAP.get(tok.tag_, "X") if lang == "lb" else tok.pos_
                     ),  # Use TAG_MAP for Luxembourgish
                     "o": tok.idx,
                 }
@@ -289,7 +321,16 @@ class LinguisticProcessing:
 
             preprocessed_text.append({"lg": lang, "tok": preprocessed_sent})
 
-        return {"ts": timestamp, "id": docid, "sents": preprocessed_text}
+        return {
+            "id": docid,
+            "ts": timestamp,
+            "sents": preprocessed_text,
+            "model_id": self.model_versions[lang],
+            "lid_path": lid_path,
+            "lingproc_git": self.git_version,
+            "char_count": full_text_len,
+            "min_chars": self.args.min_doc_length,
+        }
 
     def run(self) -> None:
         """
@@ -317,7 +358,7 @@ class LinguisticProcessing:
                         exit(1)
                     except jsonschema.SchemaError as e:
                         log.error("Schema error: %s", e)
-                        exit
+                        exit(1)
 
                 if processed_doc is not None:
                     output_doc(processed_doc, out_file)
@@ -341,7 +382,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-o", "--output-file", default="out.jsonl", help="Path to output file"
     )
-    parser.add_argument("--min-doc-length", type=int, default=200)
+    parser.add_argument("--min-doc-length", type=int, default=50)
     parser.add_argument(
         "--validate",
         action="store_true",
@@ -353,7 +394,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "--text-property",
         default="ft",
-        help="Specify the JSON property that contains the full text (default: 'ft')",
+        help="Specify the JSON property that contains the full text (%(default)s)",
+    )
+    parser.add_argument(
+        "--git-version",
+        help=(
+            "Set the git version to include in the output. If not set, the GIT_VERSION"
+            " environment variable is used."
+            "Normally the output of `git describe --tags --always` is used."
+        ),
     )
     args = parser.parse_args()
 
