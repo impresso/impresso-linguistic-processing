84bf29974fe1f0076f44833f6a525b22a34b7c84
Simon Clematide <simon.clematide@uzh.ch>
2024-11-30 00:13:34 +0100

    feat: add parallel S3 compression task to more_tasks.mk

---
+++ lib/more_tasks.mk
@@ -0,0 +1,5 @@
+## more tasks that are normally not used
+COMPRESS_S3_PATH ?= s3://41-processed-data-staging/lingproc/lingproc-pos-spacy_v3.6.0-multilingual_v1-0-3/
+parallel-s3-compress:
+	python3 lib/s3_to_local_stamps.py --list-files --list-files-glob $(COMPRESS_S3_PATH) \
+	 | parallel -j $(MAKE_PARALLEL_OPTION) python3 lib/compress_s3_key.py {} 
