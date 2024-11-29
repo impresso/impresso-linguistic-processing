fe9918f4b75779b8bff5f52e36a8042882391e6d
Simon Clematide <simon.clematide@uzh.ch>
2024-11-30 00:43:35 +0100

    feat: update parallel S3 compression task to display progress with --eta option

--- lib/more_tasks.mk
+++ lib/more_tasks.mk
@@ -2,4 +2,4 @@
 COMPRESS_S3_PATH ?= s3://41-processed-data-staging/lingproc/lingproc-pos-spacy_v3.6.0-multilingual_v1-0-3/
 parallel-s3-compress:
 	python3 lib/s3_to_local_stamps.py --list-files --list-files-glob '*.bz2' $(COMPRESS_S3_PATH) \
-	 | parallel -j $(MAKE_PARALLEL_OPTION) python3 lib/compress_s3_key.py {} 
+	 | parallel --eta python3 lib/compress_s3_key.py {} 
